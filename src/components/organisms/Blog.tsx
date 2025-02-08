2be4f3b879d98c04c92440c612dfe0602b5ea641
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2025-02-08 22:05:06 +0000

    ADD:

--- src/components/organisms/Blog.tsx
+++ src/components/organisms/Blog.tsx
@@ -1,3 +1,4 @@
+import BlogCarousel from "../molecules/BlogCarousel";
 
 
 
@@ -5,8 +6,20 @@
 const Blog = () => {
     return (
 
-        <section className="text-white" >
-            hello
+        <section className="text-white w-full flex items-center justify-center flex-col bg-red-600 " >
+          <header className=" w-full max-w-[1034px] flex items-center justify-between  font-syne mx-auto" >
+            <h1 className="max-w-[504px] font-bold text-[42px] leading-[47.88px] " >Read From <br/> Our Blog</h1>
+
+            <div>
+                button
+            </div>
+          </header>
+
+
+          <BlogCarousel/>
+
+
+
         </section>
 
     )
