e49ad46026770dd472d38d04945c9e076c798a5d
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2025-02-09 04:36:39 +0000

    mobile view

--- src/components/molecules/BlogCarousel.tsx
+++ src/components/molecules/BlogCarousel.tsx
@@ -7,6 +7,8 @@ import { Card, CardContent, CardDescription, CardHeader } from "../ui/card";impo
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import carouselImage from "../../../public/carousel-card-img.svg"
+import cardBlur from "../../../public/card-blur.svg"
+import SideBlur from "../../../public/side-blur.svg"
 import { FaArrowRight } from "react-icons/fa";
 
 
@@ -46,23 +48,25 @@ const BlogCarousel = () => {
       opts={{
         align: "start",
       }}
-      className="w-full ml-auto max-w-[1172px] flex items-center justify-center bg-orange-600 "
+      className="w-full ml-auto max-w-[1172px] flex items-center justify-center z-[999] "
     >
       <CarouselContent className=" w-full" >
         {cardDetails.map((cardDetail, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3   max-w-[335px]     ">
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3  w-[167.5px]  md:max-w-[335px] md:min-w-[290px]     ">
             <div className="p-1 ">
 
-             <Card className="bg-red-800 max-w-[335px] h-[398px] bg-[#14161F66] border border-[#1E212F] backdrop-blur-[40%] rounded-[15px] p-0 " >
-             <CardContent className= "w-full h-full flex flex-col items-center justify-stretch gap-3 rounded-[15px] px-2 py-1  " >
+             <Card className="bg-red-800 w-[167.5px] md:min-w-[290px]  md:max-w-[335px] h-[199px] md:h-[398px] bg-[#14161F66] border border-[#1E212F] backdrop-blur-[40%] rounded-[15px] p-0 " >
+             <CardContent className= "relative w-full h-full flex flex-col items-center justify-stretch gap-3 rounded-[15px] px-2 py-1  " >
               <div className="w-full h-[50%] m-0 " >
             <Image src={cardDetail.Image} alt="image" width={100} height={100} className="w-full h-full object-contain" />
               </div>
-            <CardDescription className=" w-full  h-full flex flex-col items-start justify-between py-3 "  >
-              <h1 className="text-[#ffffff] text-[15.58px] font-medium leading-[21.81px] " >{cardDetail.heading} </h1>
-              <p className="text-[#A5AEC0] text-[16.62px] font-medium leading-[24.93px] font-urbanist  " >{cardDetail.description} </p>
-              <a href={cardDetail.link} className="text-[#7A7A7A] text-[15.27px] font-semibold font-urbanist flex items-center gap-2 " > <span>Read more</span> <span><FaArrowRight/></span> </a>
+            <CardDescription className=" w-full  h-full flex flex-col items-start justify-evenly py-3 gap-2 "  >
+              <h1 className="text-[#ffffff] text-[7.7px] md:text-[15.58px] font-medium leading-[10.91px] md:leading-[21.81px] " >{cardDetail.heading} </h1>
+              <p className="text-[#A5AEC0] text-[8.31px] md:text-[16.62px] font-medium leading-[12.46px] md:leading-[24.93px] font-urbanist  " >{cardDetail.description} </p>
+              <a href={cardDetail.link} className="text-[#7A7A7A] text-[7.63px] md:text-[15.27px] font-semibold font-urbanist flex items-center gap-2 md:leading-[15.24px] leading-[7.62px] " > <span>Read more</span> <span><FaArrowRight/></span> </a>
             </CardDescription>
+            <Image src={cardBlur} alt="blur" height={100} width={100} className="absolute w-[155px] h-[143px] top-[25%] right-[15%] object-cover " />
+            <Image src={SideBlur} alt="blur" height={100} width={100} className="absolute w-[39px] h-[109px] top-[35%] left-0 object-contain  " />
              </CardContent>
              </Card>
             </div>
--- src/components/organisms/Blog.tsx
+++ src/components/organisms/Blog.tsx
@@ -1,32 +1,36 @@
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import BlogCarousel from "../molecules/BlogCarousel";
-
-
-
+import Image from "next/image";
+import orbit from "../../../public/orbit.svg"
 
 const Blog = () => {
-    return (
+  return (
+    <section className="relative text-white w-full flex items-center justify-center flex-col gap-4  px-4 md:pb-[13%]  overflow-hidden  ">
+      <header className=" w-full max-w-[1172px] flex items-center justify-between  font-syne -auto md:ml-auto pr-[5%] ">
+        <h1 className="max-w-[504px] font-bold text-[30px] md:text-[42px] leading-[30px] md:leading-[47.88px] ">
+          Read From <br /> Our Blog
+        </h1>
 
-        <section className="text-white w-full flex items-center justify-center flex-col gap-4  px-4 pb-5 overflow-hidden bg-red-500   " >
-          <header className=" w-full max-w-[1172px] flex items-center justify-between  font-syne -auto md:ml-auto pr-[5%] " >
-            <h1 className="max-w-[504px] font-bold text-[42px] leading-[47.88px] " >Read From <br/> Our Blog</h1>
+        <div className="flex flex-row items-center gap-3 ">
+          <button className="bg-transparent flex items-center justify-center w-[47.88px] h-[47.88px] text-[24px] text-[#4A4949] border-4 border-[#4A4949] hover:border-[#ffffff] hover:text-[#ffffff] transition-all duration-300 ease-in-out rounded-full ">
+            <FaChevronLeft />
+          </button>
+          <button className="bg-transparent flex items-center justify-center w-[47.88px] h-[47.88px] text-[24px] text-[#4A4949] border-4 border-[#4A4949] hover:border-[#ffffff] hover:text-[#ffffff] transition-all duration-300 ease-in-out rounded-full ">
+            <FaChevronRight />
+          </button>
+        </div>
+      </header>
 
-            <div className="flex flex-row items-center gap-3 " >
-                <button className="bg-transparent flex items-center justify-center w-[47.88px] h-[47.88px] text-[24px] text-[#4A4949] border-2 border-[#4A4949] hover:border-[#ffffff] hover:text-[#ffffff] " ><FaChevronLeft/></button>
-                <button className="bg-transparent flex items-center justify-center w-[47.88px] h-[47.88px] text-[24px] text-[#4A4949] border-2 border-[#4A4949] hover:border-[#ffffff] hover:text-[#ffffff] " ><FaChevronRight/></button>
-            </div>
-          </header>
-
-
-  
-<BlogCarousel/>
+      <BlogCarousel />
 
 
 
-
-        </section>
-
-    )
-}
+<div className="absolute bottom-0 right-[5%] w-[557.68px] h-[403.88px] hidden md:flex items-center justify-center" >
+<Image src={orbit} alt="orbit" width={100} height={100} className="h-full w-full " />
+</div>
+      
+    </section>
+  );
+};
 
 export default Blog;
