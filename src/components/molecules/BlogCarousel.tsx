cfa835f55ab0fa8e2c9045464b4c49d08c510462
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2025-02-09 02:57:55 +0000

    Created blog cards

--- src/components/molecules/BlogCarousel.tsx
+++ src/components/molecules/BlogCarousel.tsx
@@ -1,31 +1,72 @@
-import { Card, CardContent } from "@/components/ui/card"
-import {
+import Image from "next/image";
+import { Card, CardContent, CardDescription, CardHeader } from "../ui/card";import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import carouselImage from "../../../public/carousel-card-img.svg"
+import { FaArrowRight } from "react-icons/fa";
 
 
 const BlogCarousel = () => {
+
+  const cardDetails = [
+    {
+      heading: "Talk it out with audio",
+      description: "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
+      Image: carouselImage,
+      link: "link"
+    },
+    {
+      heading: "Talk it out with audio",
+      description: "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
+      Image: carouselImage,
+      link: "link"
+    },
+    {
+      heading: "Talk it out with audi",
+      description: "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
+      Image: carouselImage,
+      link: "link"
+    },
+    {
+      heading: "Talk it out with audio",
+      description: "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
+      Image: carouselImage,
+      link: "link"
+    },
+  ]
+
+
+
     return (
         <Carousel
       opts={{
         align: "start",
       }}
-      className="w-full max-w-sm"
+      className="w-full max-w-[1034px] bg-green-800"
     >
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
+        {cardDetails.map((cardDetail, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3    ">
+            <div className="p-1 ">
+
+             <Card className="bg-red-800 max-w-[335px] h-[398px] bg-[#14161F66] border border-[#1E212F] backdrop-blur-[40%] rounded-[15px] p-0 " >
+             <CardContent className= "w-full h-full flex flex-col items-center justify-stretch gap-3 bg-blue-600 rounded-[15px] px-2 py-1  " >
+              <div className="w-full h-[50%] m-0 " >
+            <Image src={cardDetail.Image} alt="image" width={100} height={100} className="w-full h-full object-contain" />
+              </div>
+            <CardDescription className=" w-full bg-green-600 h-full flex flex-col items-start justify-between py-3 "  >
+              <h1 className="text-[#ffffff] text-[15.58px] font-medium leading-[21.81px] " >{cardDetail.heading} </h1>
+              <p className="text-[#A5AEC0] text-[16.62px] font-medium leading-[24.93px] font-urbanist  " >{cardDetail.description} </p>
+              <a href={cardDetail.link} className="text-[#7A7A7A] text-[15.27px] font-semibold font-urbanist flex items-center gap-2 " > <span>Read more</span> <span><FaArrowRight/></span> </a>
+            </CardDescription>
+             </CardContent>
+             </Card>
             </div>
+
           </CarouselItem>
         ))}
       </CarouselContent>
--- src/components/organisms/Blog.tsx
+++ src/components/organisms/Blog.tsx
@@ -6,7 +6,7 @@ import BlogCarousel from "../molecules/BlogCarousel";
 const Blog = () => {
     return (
 
-        <section className="text-white w-full flex items-center justify-center flex-col bg-red-600 " >
+        <section className="text-white w-full flex items-center justify-center flex-col gap-4 bg-red-600 " >
           <header className=" w-full max-w-[1034px] flex items-center justify-between  font-syne mx-auto" >
             <h1 className="max-w-[504px] font-bold text-[42px] leading-[47.88px] " >Read From <br/> Our Blog</h1>
 
