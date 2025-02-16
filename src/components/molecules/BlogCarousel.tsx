3009b60060e3f8f9be9c8b6f09cc5a5a0e6dd5d8
Okeke Chinedu <chiscookeke11@gmail.com>
2025-02-16 06:04:08 +0100

    fixed the api type error

--- src/components/molecules/BlogCarousel.tsx
+++ src/components/molecules/BlogCarousel.tsx
@@ -1,87 +1,122 @@
 import Image from "next/image";
-import { Card, CardContent, CardDescription} from "../ui/card";import {
+import { Card, CardContent, CardDescription } from "../ui/card";
+import {
   Carousel,
   CarouselContent,
   CarouselItem,
-} from "@/components/ui/carousel"
-import carouselImage from "../../../public/carousel-card-img.svg"
-import cardBlur from "../../../public/card-blur.svg"
-import SideBlur from "../../../public/side-blur.svg"
+} from "@/components/ui/carousel";
+import carouselImage from "../../../public/carousel-card-img.svg";
+import cardBlur from "../../../public/card-blur.svg";
+import SideBlur from "../../../public/side-blur.svg";
 import { ArrowRight } from "lucide-react";
+import { CarouselApi } from "../ui/carousel";
 
+interface BlogCarouselProps {
+  setApi: (api: CarouselApi) => void; 
+}
 
-const BlogCarousel = ({ setApi }: { setApi: (api: string) => void }) => {
-
+const BlogCarousel = ({ setApi }: BlogCarouselProps) => {
   const cardDetails = [
     {
       heading: "Talk it out with audio",
-      description: "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
+      description:
+        "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
       Image: carouselImage,
-      link: "link"
+      link: "link",
     },
     {
       heading: "Talk it out with audio",
-      description: "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
+      description:
+        "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
       Image: carouselImage,
-      link: "link"
-    },
-    {
-      heading: "Talk it out with audi",
-      description: "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
-      Image: carouselImage,
-      link: "link"
+      link: "link",
     },
     {
       heading: "Talk it out with audio",
-      description: "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
+      description:
+        "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
       Image: carouselImage,
-      link: "link"
+      link: "link",
     },
     {
       heading: "Talk it out with audio",
-      description: "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
+      description:
+        "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
       Image: carouselImage,
-      link: "link"
+      link: "link",
     },
-    
-  ]
+    {
+      heading: "Talk it out with audio",
+      description:
+        "Effortlessly distribute funds to recipients, with complete transparency, robust security, and real-time tracking.",
+      Image: carouselImage,
+      link: "link",
+    },
+  ];
 
-
-
-    return (
-      <Carousel
-  setApi={setApi} 
-  opts={{
-    align: "start",
-  }}
-  className="w-full ml-auto max-w-[1172px] flex items-center justify-center z-[999]"
->
-
-      <CarouselContent className=" w-full  " >
+  return (
+    <Carousel
+      setApi={setApi} 
+      opts={{
+        align: "start",
+      }}
+      className="w-full ml-auto max-w-[1172px] flex items-center justify-center z-[999]"
+    >
+      <CarouselContent className="w-full">
         {cardDetails.map((cardDetail, index) => (
-          <CarouselItem key={index} className="basis-1/ lg:basis-1/3  w-[167.5px]   md:max-w-[335px] md:min-w-[290px] mx-2     ">
-        
-             <Card className=" w-[167.5px] md:min-w-[290px]  lg:w-[335px] h-[199px] md:h-[398px] bg-utility-blogCard border border-utility-cardBorder backdrop-blur-[40%] rounded-[15px] p-0 " >
-             <CardContent className= "relative w-full h-full flex flex-col items-center justify-stretch gap-3 rounded-[15px] px-2 py-1  " >
-              <div className="w-full h-[50%] m-0 " >
-            <Image src={cardDetail.Image} alt="image" width={100} height={100} className="w-full h-full object-contain" />
-              </div>
-            <CardDescription className=" w-full  h-full flex flex-col items-start justify-evenly py-3 gap-2 "  >
-              <h1 className="text-white text-[7.7px] md:text-[15.58px] font-medium leading-[10.91px] md:leading-[21.81px] " >{cardDetail.heading} </h1>
-              <p className="text-utility-blogDescription text-[8.31px] md:text-[16.62px] font-medium leading-[12.46px] md:leading-[24.93px] font-urbanist  " >{cardDetail.description} </p>
-              <a href={cardDetail.link} className="text-utility-blogLink text-[7.63px] md:text-[15.27px] font-semibold font-urbanist flex items-center gap-2 md:leading-[15.24px] leading-[7.62px] " > <span>Read more</span> <span><ArrowRight/></span> </a>
-            </CardDescription>
-            <Image src={cardBlur} alt="blur" height={100} width={100} className="absolute w-[155px] h-[143px] top-[25%] right-[15%] object-cover " />
-            <Image src={SideBlur} alt="blur" height={100} width={100} className="absolute w-[39px] h-[109px] top-[35%] left-0 object-contain  " />
-             </CardContent>
-             </Card>
-        
+          <CarouselItem
+            key={index}
+            className="basis-1/1 lg:basis-1/3 w-[167.5px] md:max-w-[335px] md:min-w-[290px] mx-2"
+          >
+            <Card className="w-[167.5px] md:min-w-[290px] lg:w-[335px] h-[199px] md:h-[398px] bg-utility-blogCard border border-utility-cardBorder backdrop-blur-[40%] rounded-[15px] p-0">
+              <CardContent className="relative w-full h-full flex flex-col items-center justify-stretch gap-3 rounded-[15px] px-2 py-1">
+                <div className="w-full h-[50%] m-0">
+                  <Image
+                    src={cardDetail.Image}
+                    alt="image"
+                    width={100}
+                    height={100}
+                    className="w-full h-full object-contain"
+                  />
+                </div>
+                <CardDescription className="w-full h-full flex flex-col items-start justify-evenly py-3 gap-2">
+                  <h1 className="text-white text-[7.7px] md:text-[15.58px] font-medium leading-[10.91px] md:leading-[21.81px]">
+                    {cardDetail.heading}
+                  </h1>
+                  <p className="text-utility-blogDescription text-[8.31px] md:text-[16.62px] font-medium leading-[12.46px] md:leading-[24.93px] font-urbanist">
+                    {cardDetail.description}
+                  </p>
+                  <a
+                    href={cardDetail.link}
+                    className="text-utility-blogLink text-[7.63px] md:text-[15.27px] font-semibold font-urbanist flex items-center gap-2 md:leading-[15.24px] leading-[7.62px]"
+                  >
+                    <span>Read more</span>
+                    <span>
+                      <ArrowRight />
+                    </span>
+                  </a>
+                </CardDescription>
+                <Image
+                  src={cardBlur}
+                  alt="blur"
+                  height={100}
+                  width={100}
+                  className="absolute w-[155px] h-[143px] top-[25%] right-[15%] object-cover"
+                />
+                <Image
+                  src={SideBlur}
+                  alt="blur"
+                  height={100}
+                  width={100}
+                  className="absolute w-[39px] h-[109px] top-[35%] left-0 object-contain"
+                />
+              </CardContent>
+            </Card>
           </CarouselItem>
         ))}
       </CarouselContent>
     </Carousel>
-    )
-}
+  );
+};
 
-
-export default  BlogCarousel
+export default BlogCarousel;
--- src/components/organisms/Blog.tsx
+++ src/components/organisms/Blog.tsx
@@ -1,48 +1,65 @@
-"use client"
+"use client";
+
 import BlogCarousel from "../molecules/BlogCarousel";
 import Image from "next/image";
-import orbit from "../../../public/orbit.svg"
+import orbit from "../../../public/orbit.svg";
 import { useState } from "react";
-import {  ChevronLeft, ChevronRight } from "lucide-react";
-
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { CarouselApi } from "../ui/carousel";
 
 const Blog = () => {
-const [api, setApi] = useState<EmblaCarouselType | null>(null);
 
-  const handlePrev = () => api?.scrollPrev();
-  const handleNext = () => api?.scrollNext();
+  const [api, setApi] = useState<CarouselApi | null>(null);
 
+
+  const handlePrev = () => {
+    api?.scrollPrev();
+  };
+
+  
+  const handleNext = () => {
+    api?.scrollNext();
+  };
 
   return (
-    <section className="relative text-white w-full flex items-center justify-center flex-col gap-4  px-4 md:pb-[13%]  overflow-hidden  ">
-      <header className=" w-full max-w-[1172px] flex items-center justify-between  font-syne mx-auto md:ml-auto pr-[5%] mb-[2%] ">
-        <h1 className="max-w-[504px] font-bold text-[30px] md:text-[42px] leading-[30px] md:leading-[47.88px] ">
+    <section className="relative text-white w-full flex items-center justify-center flex-col gap-4 px-4 md:pb-[13%] overflow-hidden">
+  
+      <header className="w-full max-w-[1172px] flex items-center justify-between font-syne mx-auto md:ml-auto pr-[5%] mb-[2%]">
+        <h1 className="max-w-[504px] font-bold text-[30px] md:text-[42px] leading-[30px] md:leading-[47.88px]">
           Read From <br /> Our Blog
         </h1>
 
-        <div className="flex flex-row items-center gap-3 ">
-
-
-          <button onClick={handlePrev} 
-  className="bg-transparent cursor-pointer flex items-center justify-center w-[47.88px] h-[47.88px] text-[24px] text-[darkGray] border-4 border-[darkGray] hover:border-white hover:text-white transition-all duration-300 ease-in-out rounded-full ">
-            <ChevronLeft  />
+       
+        <div className="flex flex-row items-center gap-3">
+          <button
+            onClick={handlePrev}
+            className="bg-transparent cursor-pointer flex items-center justify-center w-[47.88px] h-[47.88px] text-[24px] text-[darkGray] border-4 border-[darkGray] hover:border-white hover:text-white transition-all duration-300 ease-in-out rounded-full"
+          >
+            <ChevronLeft />
           </button>
 
-          <button  onClick={handleNext}    className="bg-transparent cursor-pointer flex items-center justify-center w-[47.88px] h-[47.88px] text-[24px] text-[darkGray] border-4 border-[darkGray] hover:border-white hover:text-white transition-all duration-300 ease-in-out rounded-full ">
+          <button
+            onClick={handleNext}
+            className="bg-transparent cursor-pointer flex items-center justify-center w-[47.88px] h-[47.88px] text-[24px] text-[darkGray] border-4 border-[darkGray] hover:border-white hover:text-white transition-all duration-300 ease-in-out rounded-full"
+          >
             <ChevronRight />
           </button>
-
         </div>
       </header>
 
+     
       <BlogCarousel setApi={setApi} />
 
-
-
-<div className="absolute bottom-0 right-[5%] w-[557.68px] h-[403.88px] hidden md:flex items-center justify-center" >
-<Image src={orbit} alt="orbit" width={100} height={100} className="h-full w-full " />
-</div>
       
+      <div className="absolute bottom-0 right-[5%] w-[557.68px] h-[403.88px] hidden md:flex items-center justify-center">
+        <Image
+          src={orbit}
+          alt="orbit"
+          width={100}
+          height={100}
+          className="h-full w-full"
+        />
+      </div>
     </section>
   );
 };
