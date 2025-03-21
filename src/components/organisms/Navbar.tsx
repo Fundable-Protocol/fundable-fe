d538ad40a4573ddf2466f0199104b5d4f76f8bd1
Verifieddanny <dannyclassic56@gmail.com>
2025-03-21 16:15:10 +0100

    Fix: Enhanced Navbar

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,18 +1,26 @@
+"use client"
 import Link from "next/link";
 import Image from "next/image";
-
+import { useState } from "react";
 import ConnectWalletButton from "@/components/atoms/Button";
 import Logo from "../../../public/imgs/fundable_logo.png";
 
-const Navbar = async () => {
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const links = [
     { title: "Solutions", url: "#" },
     { title: "Resources", url: "#" },
     { title: "About", url: "#" },
   ];
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="flex justify-between items-center py-4 text-sm font-bold text-txt container">
+      {/* Logo */}
       <Link href="/" className="relative w-28 h-5">
         <Image
           priority
@@ -23,15 +31,65 @@ const Navbar = async () => {
         />
       </Link>
 
-      <div className="hidden sm:flex gap-x-16">
+               {/* Connect Wallet Button */}
+               <div className="sm:hidden block">
+        <ConnectWalletButton />
+      </div>
+
+      {/* Hamburger Menu for Mobile */}
+      <button
+        className="sm:hidden focus:outline-none"
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+      >
+        <svg
+          className="w-6 h-6 transition-transform duration-300 ease-in-out"
+          fill="#ffffff"
+          stroke="#ffffff"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          {isMenuOpen ? (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          ) : (
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          )}
+        </svg>
+      </button>
+
+
+
+      {/* Nav Links */}
+      <div
+        className={`${
+          isMenuOpen ? "block" : "hidden"
+        } sm:flex sm:items-center sm:gap-x-16 absolute sm:static top-16 left-0 w-full sm:w-auto bg-[#000000d3] text-[#ffffff] sm:bg-transparent p-4 sm:p-0 transition-all duration-300 ease-in-out`}
+      >
         {links.map((link) => (
-          <Link href={link.url} className="text-white" key={link.title}>
+          <Link
+            href={link.url}
+            className="block sm:inline-block text-white hover:text-primary py-2 sm:py-0"
+            key={link.title}
+          >
             {link.title}
           </Link>
         ))}
       </div>
 
-      <ConnectWalletButton />
+    {/* Connect Wallet Button */}
+    <div className="hidden sm:block">
+        <ConnectWalletButton />
+      </div>
     </nav>
   );
 };
