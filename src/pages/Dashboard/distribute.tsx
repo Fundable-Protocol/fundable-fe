76e34efa4ff7dd3dd8d31b4f66d92db8a17b8e6f
yunusabdul <yunusabdulmajidyunus38@gmail.com>
2025-03-21 19:20:15 +0100

    feat: distribuion page

--- src/pages/Dashboard/distribute.tsx
+++ src/pages/Dashboard/distribute.tsx
@@ -1,5 +1,16 @@
+import { DistributionForm } from "@/components/organisms/DashboardForm";
+
 const DistributeComponent = () => {
-  return <div>DistributeComponent</div>;
+
+  return (
+    <div className="flex min-h-screen bg-[#121212] text-white">
+      <div className="flex-1 flex flex-col">
+        <main className="flex-1 p-4 overflow-auto text-white">
+          <DistributionForm />
+        </main>
+      </div>
+    </div>
+  );
 };
 
-export default DistributeComponent;
+export default DistributeComponent
---
+++ src/svg/UploadIcon.tsx
@@ -0,0 +1,43 @@
+const UploadIcon = ()=>{
+    return (
+      <svg
+        width="72"
+        height="72"
+        viewBox="0 0 72 72"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M27 58.5H20.25C16.0728 58.5 12.0668 56.8406 9.11307 53.8869C6.15937 50.9332 4.5 46.9272 4.5 42.75C4.5 38.5728 6.15937 34.5668 9.11307 31.6131C12.0668 28.6594 16.0728 27 20.25 27C21.5677 26.9995 22.8806 27.1601 24.1594 27.4781"
+          stroke="#CCE0FF"
+          strokeWidth="2.25"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M22.5 36C22.5 32.4348 23.3472 28.9207 24.9718 25.7472C26.5964 22.5737 28.9518 19.8316 31.844 17.7471C34.7362 15.6625 38.0824 14.295 41.6068 13.7573C45.1312 13.2197 48.7329 13.5272 52.1151 14.6546C55.4973 15.782 58.5632 17.697 61.0602 20.2418C63.5571 22.7865 65.4135 25.8882 66.4766 29.2912C67.5396 32.6942 67.7787 36.3011 67.1743 39.8147C66.5698 43.3282 65.1391 46.6479 63 49.5"
+          stroke="#CCE0FF"
+          strokeWidth="2.25"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M33.2139 45.5344L42.7482 36L52.2826 45.5344"
+          stroke="#8256FF"
+          strokeWidth="2.25"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M42.75 58.5V36"
+          stroke="#8256FF"
+          strokeWidth="2.25"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    );
+
+}
+
+export default UploadIcon
