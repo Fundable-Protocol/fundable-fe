3063c27503f29a28e2328e0d1db8d5d5013823c2
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2025-07-06 06:32:11 +0100

    feat: added more starknet wallect

--- src/providers/wallet-provider.tsx
+++ src/providers/wallet-provider.tsx
@@ -5,21 +5,40 @@ import { sepolia, mainnet } from "@starknet-react/chains";
 import {
   StarknetConfig,
   publicProvider,
-  // argent,
-  // braavos,
-  useInjectedConnectors,
   voyager,
 } from "@starknet-react/core";
 import cartridgeConnector from "./catridge-connector";
+import { InjectedConnector } from "starknetkit/injected";
+import { WebWalletConnector } from "starknetkit/webwallet";
+import { ArgentMobileConnector } from "starknetkit/argentMobile";
 
 const WalletProvider = ({ children }: { children: React.ReactNode }) => {
-    const { connectors } = useInjectedConnectors({
-        // recommended: [
-        //     argent(),
-        //     braavos(),
-        // ],
-        order: "random",
-    });
+  
+    const connectors = [
+      new InjectedConnector({
+        options: { id: "argentX", name: "Argent X" },
+      }),
+      new InjectedConnector({
+        options: { id: "braavos", name: "Braavos" },
+      }),
+      new InjectedConnector({
+        options: { id: "metamask", name: "MetaMask" },
+      }),
+      new InjectedConnector({
+        options: { id: "keplr", name: "Keplr" },
+      }),
+      new InjectedConnector({
+        options: { id: "okxwallet", name: "OKX" },
+      }),
+      new WebWalletConnector({ url: "https://web.argent.xyz" }),
+      ArgentMobileConnector.init({
+        options: {
+          dappName: "Fortichain", // Replace with your app's name
+          url: "https://web.argent.xyz",
+        },
+      }),
+    ];
+  
 
     return (
         <StarknetConfig
