527d35364b9a4e17e78980c6a3f789b49cc665ef
marvelousmicheal <donatusmarvelous09@gmail.com>
2025-07-24 01:21:27 +0100

    feat: implement responsive and accessible create stream page

--- src/pages/Dashboard/payment-stream.tsx
+++ src/pages/Dashboard/payment-stream.tsx
@@ -1,72 +1,168 @@
-"use client"
+import { Download, Lock } from "lucide-react";
 
-import PaymentStreamManagementTable from "@/components/organisms/PaymentStreamManagementTable";
-import { Button } from "@/components/ui/button";
-import { streamData } from "@/lib/StreamData";
-import { PlusIcon } from "lucide-react";
-import { useState } from "react";
-
-
-
-const PaymentStreamComponent = () => {
-
-
-
-  const itemsPerPage = 10;
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-
-  const totalItems = streamData.length || 0;
-const totalPages = Math.ceil(totalItems / itemsPerPage) || 1;
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page)
-  }
-
-
-  return <div className="p-4 font-urbanist">
-    <div className="  w-full h-full flex flex-col items-start gap-2" >
-      <h1 className="text-base  text-[#E1E4EA] font-semibold " >Payment streams</h1>
-      <hr className="border-[#2A2E41] border-[2px] w-full mt-[30px]  " />
-
-      <div className="w-full flex flex-col items-start "  >
-        <Button className="self-end rounded-sm px-2 md:px-[18px] py-2 md:py-[14px] bg-[#242838] border-[2px] border-[#2A2E41] gap-3 text-sm md:text-base font-semibold  " variant={"outline"}   aria-label="Create new payment stream campaign" > <PlusIcon />  <span> New Campaign</span></Button>
-        <hr className="border-[#2A2E41] border-[2px] w-full  " />
-      </div>
-
-
-      <div className="w-full flex items-center flex-col justify-center mx-auto "  >
-        <PaymentStreamManagementTable indexOfLastItem={indexOfLastItem} indexOfFirstItem={indexOfFirstItem} />
-        <div className="w-full flex items-center justify-between px-6 " >
-
-
-          <h4 className=" text-[#D9D9D980] font-normal text-sm "  >Page {currentPage} of  {totalPages} </h4>
-
-
-
-          <div className="flex items-center justify-center gap-3 py-4 "  >
-            <Button
-              onClick={() => handlePageChange(currentPage - 1)}
-              disabled={currentPage === 1}
-              variant={"outline"} className="px-4 py-[6px] rounded font-medium text-base disabled:opacity-50 " >Previous</Button>
-            <Button
-              onClick={() => handlePageChange(currentPage + 1)}
-              disabled={currentPage === totalPages }
-              variant={"outline"} className="px-4 py-[6px] rounded font-medium text-base ">Next</Button>
-          </div>
-
+export default function paymentStream() {
+  return (
+    <section className=" h-[785px] mx-[20px] bg-transparent flex gap-8">
+      <main className="bg-[#151718]/70 backdrop-blur-sm  flex-1 h-[784px] rounded-xl p-6">
+        <div className="flex flex-col gap-6">
+          <header className="pb-[30px] border-b-2 border-borderbottom">
+            <h1 className="text-base font-semibold text-link">Create Stream</h1>
+          </header>
+          <form className="flex flex-col gap-6">
+            <div className="flex gap-6 items-start">
+              <div className="flex flex-1 flex-col gap-2">
+                <label
+                  className="font-medium text-base text-[#8792AB]"
+                  htmlFor="stream-name"
+                >
+                  Name
+                </label>
+                <input
+                  placeholder="Fill in the name of the stream..."
+                  type="text"
+                  id="stream-name"
+                  className="w-full bg-[#1E212F] rounded-md px-3 py-5 placeholder:text-base placeholder:font-medium placeholder:text-[#4E5679]"
+                />
+              </div>
+              <div className="flex flex-1 flex-col gap-2">
+                <label
+                  className="font-medium text-base text-[#8792AB]"
+                  htmlFor="stream-token"
+                >
+                  Token
+                </label>
+                <div className="w-full bg-[#1E212F] rounded-md px-3  flex items-center justify-between ">
+                  <div className="w-[24px] h-[26px] rounded-[13px] bg-[#242838]"></div>
+                  <input
+                    placeholder="Choose a token..."
+                    type="text"
+                    id="stream-token"
+                    className="flex-1 mx-1 placeholder:text-base placeholder:font-medium placeholder:text-[#4E5679] bg-transparent outline-none py-5"
+                  />
+                  <button className="px-[10px] py-[8.5px] bg-[#242838] rounded text-base font-medium text-[#8792AB]">
+                    Choose
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-6 items-start">
+              <div className="flex flex-1 flex-col gap-2">
+                <label
+                  className="font-medium text-base text-[#8792AB]"
+                  htmlFor="stream-transferable"
+                >
+                  Make the stream transferable?
+                </label>
+                <input
+                  value="Transferability (on)"
+                  type="text"
+                  id="stream-transferable"
+                  className="w-full bg-[#1E212F] rounded-md px-3 py-5 placeholder:text-base placeholder:font-medium placeholder:text-[#4E5679]"
+                />
+              </div>
+              <div className="flex flex-1 flex-col gap-2">
+                <label
+                  className="font-medium text-base text-[#8792AB]"
+                  htmlFor="stream-cancellable"
+                >
+                  Make the stream cancellable?
+                </label>
+                <input
+                  value="Cancellability (on)"
+                  type="text"
+                  id="stream-cancellable"
+                  className="w-full bg-[#1E212F] rounded-md px-3 py-5 placeholder:text-base placeholder:font-medium placeholder:text-[#4E5679]"
+                />
+              </div>
+            </div>
+            <div className="flex gap-6 items-start">
+              <div className="flex flex-1 flex-col gap-2">
+                <label
+                  className="font-medium text-base text-[#8792AB]"
+                  htmlFor="stream-recipient"
+                >
+                  Recipient
+                </label>
+                <input
+                  placeholder="Fill in the address or ENS.."
+                  type="text"
+                  id="stream-recipient"
+                  className="w-full bg-[#1E212F] rounded-md px-3 py-5 placeholder:text-base placeholder:font-medium placeholder:text-[#4E5679]"
+                />
+              </div>
+              <div className="flex flex-1 flex-col gap-2">
+                <label
+                  className="font-medium text-base text-[#8792AB]"
+                  htmlFor="stream-token"
+                >
+                  Streaming Duration
+                </label>
+                <div className="flex w-full items-center gap-6 ">
+                  <input
+                    value="Month (30d)"
+                    type="text"
+                    id="stream-duration"
+                    className="flex-1 bg-[#1E212F] rounded-md px-3 py-5 placeholder:text-base placeholder:font-medium placeholder:text-[#4E5679]"
+                  />
+                  <input
+                    value="1"
+                    type="text"
+                    id="stream-amount"
+                    className="w-[61px] bg-[#1E212F] rounded-md px-3 py-5 placeholder:text-base placeholder:font-medium placeholder:text-[#4E5679]"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-4 items-center justify-end">
+              <button className="flex items-center gap-3 px-[18px] py-[14px] bg-[#2A2E41] rounded text-base font-semibold text-[#E1E4EA] leading-4">
+                <Download className="w-5 h-5 text-gray-600" />
+                Save Draft
+              </button>
+              <button className="flex items-center gap-3 px-[18px] py-[14px] rounded text-base font-semibold text-white leading-4 bg-[linear-gradient(to_right,_#1F8EBE_0%,_#440495_39%,_#440495_69%,_#B102CD_98%)]">
+                Continue
+                <Lock className="w-5 h-5 " />
+              </button>
+              
+            </div>
+          </form>
         </div>
-      </div>
+      </main>
+      <main className="bg-[#151718] w-[311px] h-[370px] rounded-xl p-6">
+         <div className="flex flex-col gap-6">
+          <header className="pb-[30px] border-b-2 border-borderbottom">
+            <h1 className="text-base font-semibold text-link">Summary</h1>
+          </header>
+          <div className="flex flex-col gap-6">
+            <article className="flex justify-between items-center ">
+              <h5 className="font-semibold text-base text-[#8792AB]">Name</h5>
+              <p className="font-medium text-base text-[#4E5679]">First Stream</p>
+            </article>
+            <article className="flex justify-between items-center ">
+              <h5 className="font-semibold text-base text-[#8792AB]">Token</h5>
+              <p className="font-medium text-base text-[#4E5679]">First Stream</p>
+            </article>
+            <article className="flex justify-between items-center ">
+              <h5 className="font-semibold text-base text-[#8792AB]">Streambility</h5>
+              <p className="font-medium text-base text-[#4E5679]">ON</p>
+            </article>
+            <article className="flex justify-between items-center ">
+              <h5 className="font-semibold text-base text-[#8792AB]">Cancellability</h5>
+              <p className="font-medium text-base text-[#4E5679]">OFF</p>
+            </article>
+            <article className="flex justify-between items-center ">
+              <h5 className="font-semibold text-base text-[#8792AB]">Recipient</h5>
+              <p className="font-medium text-base text-[#4E5679]">0x000........987</p>
+            </article>
+            <article className="flex justify-between items-center ">
+              <h5 className="font-semibold text-base text-[#8792AB]">Streaming Duration</h5>
+              <p className="font-medium text-base text-[#4E5679]">Weekly</p>
+            </article>
+          </div>
+          </div>
+      </main>
+    </section>
+  );
+}
 
 
 
-
-    </div>
-
-
-  </div>;
-};
-
-export default PaymentStreamComponent;
